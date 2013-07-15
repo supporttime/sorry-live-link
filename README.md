4980dd708739c02f98a04b1b81e60afc6e60efb3
Robert Rawlins <robert@supporttime.com>
2013-07-15 14:32:06 +0100

    Added the initial draft of the liv, very rough but works none the less.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 sorry-live-link
 ===============
 
-A small JS plugin which turns your Sorry status page links in to a live message based on your pages real status.
+This is a small and basic draft for a jQuery dependant JS snippet which turns a static status page link in to something dynamic which displays a message based on the real status of the taget page.
+
+To use this simple drop the lib in to your chosen project and add a class to your status page link.
+
+```html
+	<a href="http://{{your-subdomain}}.sorryapp.com/" class="live-status">View Our Service Status</a>
+```
---
+++ jquery.sorry-live-link.js
@@ -0,0 +1,48 @@
+/* ===========================================================
+ * sorry.live-status-link.ks
+ * A small snippet which allows the easy creation of a live status
+ * link which displays a message which directly corsponds to the
+ * real status of the relevent page.
+ * 
+ * This intended for use with Sorryapp.com status page API and
+ * is not permitted for use with any other status page provider.
+ * ========================================================== */
+
+// Ensure that the jQuery dependancy is ready to go.
+$(document).ready(function() {
+  // Lopp the collection link links we are to make live.
+  $('a.live-status').each(function(index, object) {
+  	// Get a refernce to the link for easy access.
+  	var this_link = $(this);
+
+  	// Make an AJAX request for the status of think page.
+  	// NOTE: Is an AJAX call the best option here, since it's just a simple GET request?
+  	$.ajax({
+	  type: "GET",
+      dataType: "json",
+      crossDomain: true,	  
+	  url: this_link.attr('href'), // Use the href of the status page link.
+	  data: {
+	  	format: 'json' // Not sure why this isn't done by default, maybe cross-domain bug?
+	  },
+	  success: function(data, textStatus, jqXHR) {
+	  	// Fired when the AJAX request for status works just fine.
+	  	// We display an approproriate message based on the status of the page.
+	  	if (data.current_apologies.length == 0) {
+	  		// TODO: These classes and messages should be configurable.
+	  		// Add a class to state that all is well.
+	  		this_link.addClass('all-is-well');
+	  		// The status is good, set the appropriate link.
+	  		this_link.html('Current Status: <span>All Is Well.</span>');
+	  	} else {
+	  		// TODO: These classes and messages should be configurable.
+	  		// Add a class for a bad result.
+	  		this_link.addClass('sorry');
+	  		// We have a problem, display the appropriate link message.
+	  		this_link.html('Current Status: <span>Sorry, We Have A Problem.</span>');
+	  	};
+	  }
+	  // TODO: We need to handle failures and exceptions - even if it is doing so quietly etc.
+	});
+  });
+});
